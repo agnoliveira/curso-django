e2e74c65ac9d6afd62a2783f630810417669a487
agnoliveira <agnoliveira@gmail.com>
2020-12-07 10:32:56 -0300

    Adicionado configuração para SECRET_KEY
    close #23

--- guinadjango/settings.py
+++ guinadjango/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o!n2x3j%dm3pvt_)=_-yxl(j)x0v^cw-6$_iq81(($&d%x3g8y'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
